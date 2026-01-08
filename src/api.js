569f775e7fbe5d3af41b71a328a0905113593412
Tosin Olajumoke <134648814+TosinOlajumoke@users.noreply.github.com>
2026-01-08 01:27:49 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,3 +1,3 @@
 // Centralized API configuration file
 export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "https://endonursevr-lms-backend.onrender.com/";
+  import.meta.env.VITE_API_BASE_URL || "https://endonursevr-lms-backend.onrender.com";
